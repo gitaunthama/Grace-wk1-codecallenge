39d3ed6c35ffeebb6c04ba67478b6c6342e20924
gitaunthama <gitaunthama@gmail.com>
2024-04-26 21:09:44 +0300

    search bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <div id="body">
+        <h1>The Royal Bank of Flat-iron</h1>
+    </div>
+  )
+}
+
+export default Navbar
